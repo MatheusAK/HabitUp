c82382f1a2faa3c45ee3a92148b5a29652886af1
MatheusAK <matkut2010@gmail.com>
2026-05-23 14:28:03 -0300

    Versão 0.1 finalizada

--- mobile/src/main.tsx
+++ mobile/src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './app'
-import './mobile-styles.css' // ← usa esse em vez do ../../src/styles.css
+import '../../src/styles.css' // ← usa o original diretamente
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
